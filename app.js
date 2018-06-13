f2a8ad27a8131b3800fa121f82048e420159b895
khayri <khayribattikh@gmail.com>
2018-06-13 10:42:38 -0700

    test 1

---
+++ app.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+    res.send('<h1>Hello World</h1>');
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function(socket){
+    console.log('Un client est maintenant connecté !');
+    socket.on('pseudo',function(pseudo){
+        socket.pseudo = pseudo;
+    });
+    socket.on('message',function(message){
+        socket.broadcast.emit('message',{from:socket.pseudo , contenu:message});
+    })
+});
+
+
+server.listen(process.env.PORT ||1234);
